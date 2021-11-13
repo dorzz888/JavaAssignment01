6717ea5d3cd28187cb6853b79fd225de44c034db
dorzz888 <dorsa.n@outlook.com>
2021-11-13 13:09:16 +0100

    main class wip...

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        int[] userArr = Sort.GetNumbers();
+        Sort.Sorting(userArr);
 
     }
 }
